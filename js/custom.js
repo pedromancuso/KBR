d11652bfd924e76563192831b836b93780b3d7a9
Agu Marciali <agustin.marciali@gmail.com>
2022-05-18 21:52:26 -0300

    modal login
    Se agrego la logica del login y logout

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
-var user="Admin";
-var pass="Admin";
+var user = "admin@";
+var pass = "admin";
 
 /*
 // google map
@@ -38,220 +38,339 @@ function loadGoogleMap(){
 // Flexslider
 $(function(){
   /* FlexSlider */
- /* $('.flexslider').flexslider({
-      animation: "fade",
-      directionNav: false
-  });
+/* $('.flexslider').flexslider({
+     animation: "fade",
+     directionNav: false
+ });
 
-  new WOW().init();
+ new WOW().init();
 
-  loadGoogleMap();
+ loadGoogleMap();
 });
 
 // isotope
 jQuery(document).ready(function($){
 
-  if ( $('.iso-box-wrapper').length > 0 ) { 
+ if ( $('.iso-box-wrapper').length > 0 ) { 
 
-      var $container  = $('.iso-box-wrapper'), 
-        $imgs     = $('.iso-box img');
+     var $container  = $('.iso-box-wrapper'), 
+       $imgs     = $('.iso-box img');
 
-      $container.imagesLoaded(function () {
+     $container.imagesLoaded(function () {
 
-        $container.isotope({
-        layoutMode: 'fitRows',
-        itemSelector: '.iso-box'
-        });
+       $container.isotope({
+       layoutMode: 'fitRows',
+       itemSelector: '.iso-box'
+       });
 
-        $imgs.load(function(){
-          $container.isotope('reLayout');
-        })
+       $imgs.load(function(){
+         $container.isotope('reLayout');
+       })
 
-      });
+     });
 
-      //filter items on button click
-      $('.filter-wrapper li a').click(function(){
+     //filter items on button click
+     $('.filter-wrapper li a').click(function(){
 
-          var $this = $(this), filterValue = $this.attr('data-filter');
+         var $this = $(this), filterValue = $this.attr('data-filter');
 
-      $container.isotope({ 
-        filter: filterValue,
-        animationOptions: { 
-            duration: 750, 
-            easing: 'linear', 
-            queue: false, 
-        }                
-      });             
+     $container.isotope({ 
+       filter: filterValue,
+       animationOptions: { 
+           duration: 750, 
+           easing: 'linear', 
+           queue: false, 
+       }                
+     });             
 
-      // don't proceed if already selected 
-      if ( $this.hasClass('selected') ) { 
-        return false; 
-      }
+     // don't proceed if already selected 
+     if ( $this.hasClass('selected') ) { 
+       return false; 
+     }
 
-      var filter_wrapper = $this.closest('.filter-wrapper');
-      filter_wrapper.find('.selected').removeClass('selected');
-      $this.addClass('selected');
+     var filter_wrapper = $this.closest('.filter-wrapper');
+     filter_wrapper.find('.selected').removeClass('selected');
+     $this.addClass('selected');
 
-        return false;
-      }); 
+       return false;
+     }); 
 
-  }
+ }
 
 });
 
 // Hide mobile menu after clicking on a link
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+   $('.navbar-collapse a').click(function(){
+       $(".navbar-collapse").collapse('hide');
+   });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function() {
-        $('.navbar-default a, a,').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 68
-            }, 1000);
-            event.preventDefault();
-        });
-    });
+   $(function() {
+       $('.navbar-default a, a,').bind('click', function(event) {
+           var $anchor = $(this);
+           $('html, body').stop().animate({
+               scrollTop: $($anchor.attr('href')).offset().top - 68
+           }, 1000);
+           event.preventDefault();
+       });
+   });
 
 */
-  consultoriaboton.addEventListener("click", consultoriain);
-  consultoria.addEventListener("click", consultoriaout);
-  function consultoriain(){
-    document.getElementById("consultoria").style.display = "block";
-    document.getElementById("detalle").style.display = "block";
-    document.getElementById("service").style.display = "none";
-    document.getElementById("consultoriai").className = "fa fa-arrow-left";
-  }
-  function consultoriaout(){
-    document.getElementById("consultoria").style.display = "none";
-    document.getElementById("detalle").style.display = "none";
-    document.getElementById("service").style.display = "block";
-    document.getElementById("consultoriai").className = "fa fa-laptop";
+consultoriaboton.addEventListener("click", consultoriain);
+consultoria.addEventListener("click", consultoriaout);
+function consultoriain() {
+  document.getElementById("consultoria").style.display = "block";
+  document.getElementById("detalle").style.display = "block";
+  document.getElementById("service").style.display = "none";
+  document.getElementById("consultoriai").className = "fa fa-arrow-left";
+}
+function consultoriaout() {
+  document.getElementById("consultoria").style.display = "none";
+  document.getElementById("detalle").style.display = "none";
+  document.getElementById("service").style.display = "block";
+  document.getElementById("consultoriai").className = "fa fa-laptop";
+}
+
+soporteboton.addEventListener("click", soportein);
+soporte.addEventListener("click", soporteout);
+function soportein() {
+  document.getElementById("soporte").style.display = "block";
+  document.getElementById("detalle").style.display = "block";
+  document.getElementById("service").style.display = "none";
+  document.getElementById("soportei").className = "fa fa-arrow-left";
+}
+function soporteout() {
+  document.getElementById("soporte").style.display = "none";
+  document.getElementById("detalle").style.display = "none";
+  document.getElementById("service").style.display = "block";
+  document.getElementById("soportei").className = "fa fa-cog";
+}
+
+telefoniaipboton.addEventListener("click", telefoniaipin);
+telefoniaip.addEventListener("click", telefoniaipout);
+function telefoniaipin() {
+  document.getElementById("telefoniaip").style.display = "block";
+  document.getElementById("detalle").style.display = "block";
+  document.getElementById("service").style.display = "none";
+  document.getElementById("telefoniaipi").className = "fa fa-arrow-left";
+  document.getElementById("telefoniaipi").textContent = "";
+}
+function telefoniaipout() {
+  document.getElementById("telefoniaip").style.display = "none";
+  document.getElementById("detalle").style.display = "none";
+  document.getElementById("service").style.display = "block";
+  document.getElementById("telefoniaipi").className = "fa material-icons";
+  document.getElementById("telefoniaipi").textContent = "settings_phone";
+}
+
+servidoresboton.addEventListener("click", servidoresin);
+servidores.addEventListener("click", servidoresout);
+function servidoresin() {
+  document.getElementById("servidores").style.display = "block";
+  document.getElementById("detalle").style.display = "block";
+  document.getElementById("service").style.display = "none";
+  document.getElementById("servidoresi").className = "fa fa-arrow-left";
+}
+function servidoresout() {
+  document.getElementById("servidores").style.display = "none";
+  document.getElementById("detalle").style.display = "none";
+  document.getElementById("service").style.display = "block";
+  document.getElementById("servidoresi").className = "fa fa-server";
+}
+
+redesboton.addEventListener("click", redesin);
+redes.addEventListener("click", redesout);
+function redesin() {
+  document.getElementById("redes").style.display = "block";
+  document.getElementById("detalle").style.display = "block";
+  document.getElementById("service").style.display = "none";
+  document.getElementById("redesi").className = "fa fa-arrow-left";
+  document.getElementById("redesi").textContent = "";
+}
+function redesout() {
+  document.getElementById("redes").style.display = "none";
+  document.getElementById("detalle").style.display = "none";
+  document.getElementById("service").style.display = "block";
+  document.getElementById("redesi").className = "fa material-icons";
+  document.getElementById("redesi").textContent = "router";
+}
+
+desarrollosboton.addEventListener("click", desarrollosin);
+desarrollos.addEventListener("click", desarrollosout);
+function desarrollosin() {
+  document.getElementById("desarrollos").style.display = "block";
+  document.getElementById("detalle").style.display = "block";
+  document.getElementById("service").style.display = "none";
+  document.getElementById("desarrollosi").className = "fa fa-arrow-left";
+}
+function desarrollosout() {
+  document.getElementById("desarrollos").style.display = "none";
+  document.getElementById("detalle").style.display = "none";
+  document.getElementById("service").style.display = "block";
+  document.getElementById("desarrollosi").className = "fa fa-code";
+}
+document.getElementById('login').addEventListener('click', accion);
+
+function accion() {
+  var val = document.getElementById("usuario").value;
+  document.getElementById("requisito").innerHTML = val.lastIndexOf("@");
+
+  if (val.lastIndexOf("@") <= 0) {
+    document.getElementById("requisito").innerHTML = "No pusiste un correo válido";
+
+  } else {
+    document.getElementById("requisito").innerHTML = "El correo es válido";
   }
 
-  soporteboton.addEventListener("click", soportein);
-  soporte.addEventListener("click", soporteout);
-  function soportein(){
-    document.getElementById("soporte").style.display = "block";
-    document.getElementById("detalle").style.display = "block";
-    document.getElementById("service").style.display = "none";
-    document.getElementById("soportei").className = "fa fa-arrow-left";
-  }
-  function soporteout(){
-    document.getElementById("soporte").style.display = "none";
-    document.getElementById("detalle").style.display = "none";
-    document.getElementById("service").style.display = "block";
-    document.getElementById("soportei").className = "fa fa-cog";
+  console.log(val.lastIndexOf("@"));
+}
+
+var logged = false;
+document.getElementById('login').addEventListener('click', accion1);
+function accion1() {
+  var val = document.getElementById("usuario").value;
+  //document.getElementById("requisito").innerHTML = val.lastIndexOf("@");
+
+  if (val == user) {
+    document.getElementById("requisito").innerHTML = "Usuario Correcto";
+
+  } else {
+    document.getElementById("requisito").innerHTML = "Usuario Incorrecto";
+    var errores = 1;
+    console.log(errores);
   }
 
-  telefoniaipboton.addEventListener("click", telefoniaipin);
-  telefoniaip.addEventListener("click", telefoniaipout);
-  function telefoniaipin(){
-    document.getElementById("telefoniaip").style.display = "block";
-    document.getElementById("detalle").style.display = "block";
-    document.getElementById("service").style.display = "none";
-    document.getElementById("telefoniaipi").className = "fa fa-arrow-left";
-    document.getElementById("telefoniaipi").textContent = "";
-  }
-  function telefoniaipout(){
-    document.getElementById("telefoniaip").style.display = "none";
-    document.getElementById("detalle").style.display = "none";
-    document.getElementById("service").style.display = "block";
-    document.getElementById("telefoniaipi").className = "fa material-icons";
-    document.getElementById("telefoniaipi").textContent = "settings_phone";
+  console.log(val);
+}
+
+document.getElementById('login').addEventListener('click', accion2);
+function accion2() {
+  var val = document.getElementById("contraseña").value;
+  //document.getElementById("requisito").innerHTML = val.lastIndexOf("@");
+
+  if (val == pass) {
+    document.getElementById("requisito2").innerHTML = "Contraseña Correcta";
+    console.log(logged);
+  } else {
+    document.getElementById("requisito2").innerHTML = "Contraseña Incorrecta";
+
   }
 
-  servidoresboton.addEventListener("click", servidoresin);
-  servidores.addEventListener("click", servidoresout);
-  function servidoresin(){
-    document.getElementById("servidores").style.display = "block";
-    document.getElementById("detalle").style.display = "block";
-    document.getElementById("service").style.display = "none";
-    document.getElementById("servidoresi").className = "fa fa-arrow-left";
-  }
-  function servidoresout(){
-    document.getElementById("servidores").style.display = "none";
-    document.getElementById("detalle").style.display = "none";
-    document.getElementById("service").style.display = "block";
-    document.getElementById("servidoresi").className = "fa fa-server";
-  }
+  console.log(val);
+}
 
-  redesboton.addEventListener("click", redesin);
-  redes.addEventListener("click", redesout);
-  function redesin(){
-    document.getElementById("redes").style.display = "block";
-    document.getElementById("detalle").style.display = "block";
-    document.getElementById("service").style.display = "none";
-    document.getElementById("redesi").className = "fa fa-arrow-left";
-    document.getElementById("redesi").textContent = "";
-  }
-  function redesout(){
-    document.getElementById("redes").style.display = "none";
-    document.getElementById("detalle").style.display = "none";
-    document.getElementById("service").style.display = "block";
-    document.getElementById("redesi").className = "fa material-icons";
-    document.getElementById("redesi").textContent = "router";
-  }
-
-  desarrollosboton.addEventListener("click", desarrollosin);
-  desarrollos.addEventListener("click", desarrollosout);
-  function desarrollosin(){
-    document.getElementById("desarrollos").style.display = "block";
-    document.getElementById("detalle").style.display = "block";
-    document.getElementById("service").style.display = "none";
-    document.getElementById("desarrollosi").className = "fa fa-arrow-left";
-  }
-  function desarrollosout(){
-    document.getElementById("desarrollos").style.display = "none";
-    document.getElementById("detalle").style.display = "none";
-    document.getElementById("service").style.display = "block";
-    document.getElementById("desarrollosi").className = "fa fa-code";
-  }
-  document.getElementById('imprimir').addEventListener('click',accion);
-
-  function accion() {
-    var val=document.getElementById("texto-modal").value;
-    if(val==2){
-      document.getElementById("div2").innerHTML="Pustiste un 2";
-
-    }else{
-      document.getElementById("div2").innerHTML="NO Pustiste un 2"; 
-    }
-    
-    console.log(val);
-  }
-  
-  // Get the modal
-var modal = document.getElementById("myModal");
-
+// Get the modal
+var modal = document.getElementById("modal-login");
 // Get the button that opens the modal
-var btn = document.getElementById("ticket-boton");
+var btn = document.getElementById("login-boton");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-// 1. añadir texto que diga ingrese credenciales
-// 2. añadir input y leyendas que correspondan placeholders (el input de la contraseña que no se vea)
-// 3. añadir un boton que envíe los valores de los imput a una función
-// 4. si las credenciales ingresadas son las correctas, mostar mensaje de exito sino lo contrario
+
+
+
+
+
+
+
+function clickCounter() {
+
+  //document.getElementById('login').addEventListener('click', accion2);
+  //function accion2() {
+  var val = document.getElementById("contraseña").value;
+  //document.getElementById("requisito").innerHTML = val.lastIndexOf("@");
+  if (val == pass) {
+    //document.getElementById("requisito2").innerHTML = "Contraseña Correcta";
+    var val = document.getElementById("usuario").value;
+    if (val != user) {
+      var error = 1;
+      if (typeof (Storage) !== "undefined") {
+        if (sessionStorage.clickcount) {
+          sessionStorage.clickcount = Number(sessionStorage.clickcount) + error;
+        } else {
+          sessionStorage.clickcount = -1;
+        }
+        var intentos = 2 - (sessionStorage.clickcount) - 1;
+        document.getElementById("result").innerHTML = "Te quedan " + intentos + " intentos.";
+        if (intentos <= 0) {
+          modal.style.display = "none";
+        }
+      } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+      }
+    } else {
+      sessionStorage.clear();
+      var error = 0;
+      logged = true;
+      //console.log(intentos);
+      //console.log("clickcount");
+      const myTimeout = setTimeout(myGreeting, 2000);
+      function myGreeting() {
+        modal.style.display = "none";
+        document.getElementById("login-boton").innerHTML = "LOGOUT";
+        document.getElementById("login-boton").style.color = "red";
+        document.getElementById('login-boton').addEventListener('click', accion22);
+        function accion22() {
+          document.getElementById('usuario').value = '';
+          document.getElementById('contraseña').value = '';
+          document.getElementById("login-boton").innerHTML = "LOGIN";
+          document.getElementById("login-boton").style.color = "";
+          document.getElementById("requisito").innerHTML = "El usuario es el mail registrado";
+          document.getElementById("requisito2").innerHTML = "Debe incluir alguna mayúscula y número";
+          document.getElementById("result").innerHTML = "";
+          if (logged) {
+            modal.style.display = "none";
+          }
+          logged = false;
+        }
+      }
+    } 
+  
+  } else {
+    var error = 1;
+    if (typeof (Storage) !== "undefined") {
+      if (sessionStorage.clickcount) {
+        sessionStorage.clickcount = Number(sessionStorage.clickcount) + error;
+      } else {
+        sessionStorage.clickcount = -1;
+      }
+      var intentos = 2 - (sessionStorage.clickcount) - 1;
+      document.getElementById("result").innerHTML = "Te quedan " + intentos + " intentos.";
+      if (intentos == 0) {
+        modal.style.display = "none";
+      }
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+  }
+
+}
+
+
+
+
+
+// 1. añadir texto que diga ingrese credenciales check
+// 2. añadir input y leyendas que correspondan placeholders (el input de la contraseña que no se vea) check
+// 3. añadir un boton que envíe los valores de los imput a una función check
+// 4. si las credenciales ingresadas son las correctas, mostar mensaje de exito sino lo contrario check
 // 5. poner un contador de fracaso y si llegó al máximo cerrar el modal
 // 6. si las credenciales son correctas cerrar el modal luego del mens 
